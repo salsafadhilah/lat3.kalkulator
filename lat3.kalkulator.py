8524df30ceb8f933b098e6dbabbf1e992df49444
salsafadhilah <37093417+salsafadhilah@users.noreply.github.com>
2018-04-16 00:53:13 -0700

    Add files via upload

---
+++ lat3.kalkulator.py
@@ -0,0 +1,16 @@
+print"      KALKULATOR \n"
+print"=========================\n\n"
+
+def fungsi (nilai1,nilai2,tugas):
+    if tugas=="+":
+        hasil= nilai1+nilai2
+    elif tugas =="-":
+        hasil= nilai1-nilai2
+    elif tugas=="*":
+        hasil= nilai1*nilai2
+    elif tugas== "/":
+        hasil= nilai1/nilai2
+
+        return hasil
+    
+
